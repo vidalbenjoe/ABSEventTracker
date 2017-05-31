bdc87a450989be78f59b97df74bcfc7eeb023d64
Benjoe Vidal <benjoe22@gmail.com>
2017-05-31 11:13:12 +0800

    Added Event Qualifier

---
+++ EventApp/ABSEventAttributeQualifier.h
@@ -0,0 +1,13 @@
+//
+//  ABSEventAttributeQualifier.h
+//  EventApp
+//
+//  Created by Flydubai on 31/05/2017.
+//  Copyright © 2017 ABS-CBN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ABSEventAttributeQualifier : NSObject
+
+@end
---
+++ EventApp/ABSEventAttributeQualifier.m
@@ -0,0 +1,13 @@
+//
+//  ABSEventAttributeQualifier.m
+//  EventApp
+//
+//  Created by Flydubai on 31/05/2017.
+//  Copyright © 2017 ABS-CBN. All rights reserved.
+//
+
+#import "ABSEventAttributeQualifier.h"
+
+@implementation ABSEventAttributeQualifier
+
+@end
