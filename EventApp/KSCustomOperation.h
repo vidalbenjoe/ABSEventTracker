2a7c0be49c29c5c49656e6e7380207bca0894f52
Benjoe Vidal <benjoe22@gmail.com>
2017-06-22 21:31:59 +0800

    Added Custom NSOperation for task dispatcher

---
+++ EventApp/KSCustomOperation.h
@@ -0,0 +1,13 @@
+//
+//  KSCustomOperation.h
+//  EventApp
+//
+//  Created by Flydubai on 22/06/2017.
+//  Copyright © 2017 ABS-CBN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KSCustomOperation : NSOperation
+
+@end
---
+++ EventApp/KSCustomOperation.m
@@ -0,0 +1,13 @@
+//
+//  KSCustomOperation.m
+//  EventApp
+//
+//  Created by Flydubai on 22/06/2017.
+//  Copyright © 2017 ABS-CBN. All rights reserved.
+//
+
+#import "KSCustomOperation.h"
+
+@implementation KSCustomOperation
+
+@end
